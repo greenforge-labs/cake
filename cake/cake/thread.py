1178931df7de18fbffe474d8c1a6714c7e2abb70
Alistair English <alistair.e@greenforgelabs.com.au>
2025-11-05 08:09:47 +1000

    add python timers

--- cake/cake/thread.py
+++ cake/cake/thread.py
@@ -2,10 +2,12 @@ import threading
 
 from .context import Context
 
-from typing import Any, Callable
+from typing import Callable, TypeVar
+
+ContextT = TypeVar("ContextT", bound=Context)
 
 
-def create_thread(context: Context, func: Callable[[Any], None]):
+def create_thread(context: ContextT, func: Callable[[ContextT], None]):
     thread = threading.Thread(target=func, args=(context,))
     thread.start()
     context.threads.append(thread)
